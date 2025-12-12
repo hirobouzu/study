18b6bdac9bdb97fa36644a8882694d93884ebb68
hirobouzu <iso20023@gmail.com>
2025-12-12 16:51:11 +0900

    Create kurohon-4,5.java

---
+++ Kuro5Main.java
@@ -0,0 +1,11 @@
+package kuro5;
+
+import java.util.List;
+
+public class Kuro5Main {
+	public static void main(String[] args) {
+		List<String>list = List.of("A","B","C");
+		list.forEach(str -> System.out.println(str));
+	}
+
+}
